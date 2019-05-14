c1f4e7d66c1d775a56872769429db18143affd13
宋立杰 <songlijie@192.168.2.101>
2019-05-14 15:10:28 +0800

    mac version

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async } from '@angular/core/testing';
+1import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
